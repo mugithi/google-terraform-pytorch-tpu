bd5993e6a5c7d94f9f42dbc03e0e839b37cbff73
mugithi <isackaranja@gmail.com>
2020-06-14 20:25:29 +0000

    MIG working

--- env_setup/scripts/setup_mig.sh
+++ env_setup/scripts/setup_mig.sh
@@ -21,8 +21,11 @@ PROJECT_ID=
 ENV_BUILD_NAME=
 
 sudo gsutil cp gs://${PROJECT_ID}-${ENV_BUILD_NAME}-tf-backend/workspace/values.env /tmp/
+sudo gsutil cp gs://${PROJECT_ID}-${ENV_BUILD_NAME}-tf-backend/workspace/values.env.auto /tmp/
 
 source /tmp/values.env
+MIG=$MACHINE_TYPE-$ENV_BUILD_NAME-mig
+MIG_MASTER=$(gcloud compute instance-groups list-instances $MIG --zone $ZONE --format="value(instance.scope().segment(2))" --limit=1)
 
 SHARED_NFS_IP=$(gcloud filestore instances describe $PROJECT_ID-$ENV_BUILD_NAME-filestore --zone $ZONE --format="value(networks.ipAddresses[0])")
 
@@ -34,9 +37,36 @@ sudo mount ${SHARED_NFS_IP}:/${SHARED_FS} $MOUNT_POINT/nfs_share
 echo "${SHARED_NFS_IP}:/${SHARED_FS} $MOUNT_POINT/nfs_share nfs      defaults    0       0" | sudo tee -a /etc/fstab  
 sudo chmod go+rw $MOUNT_POINT/nfs_share
 
-## Download models to the NFS share
-mkdir -p $MOUNT_POINT/nfs_share/models/
-sudo gsutil cp -r  gs://${PROJECT_ID}-${ENV_BUILD_NAME}-tf-backend/workspace/models/* $MOUNT_POINT/nfs_share/models/
+## Download models folder to the NFS share, runs on only one host   
+if [[ $HOSTNAME == $MIG_MASTER ]]
+then 
+    mkdir -p $MOUNT_POINT/nfs_share/models/
+    sudo gsutil cp -r  gs://${PROJECT_ID}-${ENV_BUILD_NAME}-tf-backend/workspace/models/* $MOUNT_POINT/nfs_share/models/
+fi 
 
+## Download setup shared persistant disk, runs in all the hosts  
+## If the shared PD is not attached, exit the script 
+if [[ ! -b /dev/disk/by-id/google-shared-pd ]] 
+then 
+    echo -e "${RED}The shared PD is not attached to the managed instance group ${NC}"
+    echo -e "${RED}Edit the ${GREEN}values.env${RED} file and change the ${GREEN}SHARED_PD_DISK_ATTACH${RED} value and change the value from ${GREEN}false${NC} to true${NC}"
+    echo -e "${RED}Run rerun the ${GREEN}gcloud builds submit --config=cloudbuild.yaml . --substitutions _BUILD_ACTION=update,_MIG=true${RED} again ${NC}"
+    echo -e "${RED}Then attempt reruning the ${GREEN}data_prep_seed_shared_disk_pd.sh${NC}"
+    exit 0
+## If the shared is attached, and existing mount folder, reuse that folder 
+elif [[ -b /dev/disk/by-id/google-shared-pd ]] && [[ -d ${MOUNT_POINT}/shared_pd ]] 
+then 
+    echo -e "${RED}Using existing disk ${NC}"
+    sudo mount -o discard,defaults /dev/disk/by-id/google-shared-pd ${MOUNT_POINT}/shared_pd
+    sudo chmod a+w ${MOUNT_POINT}/shared_pd
+## If the shared is attached no  folder mount folder create  
+elif [[ -b /dev/disk/by-id/google-shared-pd ]] && [[ ! -d ${MOUNT_POINT}/shared_pd ]]
+then  
+    sudo mkdir -p ${MOUNT_POINT}/shared_pd 
+    sudo mount -o discard,defaults /dev/disk/by-id/google-shared-pd ${MOUNT_POINT}/shared_pd
+    sudo chmod a+w ${MOUNT_POINT}/shared_pd
+fi 
+
+ 
 ## Download Docker Containers 
 #docker pull gcr.io/tpu-pytorch/xla:nightly_3.6
--- models/roberta/env_setup/data_prep_seed_shared_disk_pd.sh
+++ models/roberta/env_setup/data_prep_seed_shared_disk_pd.sh
@@ -15,16 +15,6 @@
 
 source /tmp/values.env
 
-## If the shared PD is not attached, exit the script 
-if [[ ! -f /dev/disk/by-id/google-shared-pd ]] 
-then 
-    echo -e "${RED}The shared PD is not attached to the managed instance group ${NC}"
-    echo -e "${RED}Edit the values.env file and change the ${NC}SHARED_PD_DISK_ATTACH=false${RED} to true ${NC}"
-    echo -e "${RED}Run rerun the ${NC}gcloud builds submit --config=cloudbuild.yaml . --substitutions _BUILD_ACTION=update,_MIG=true${RED} again ${NC}"
-    echo -e "${RED}Then attempt reruning the ${NC}data_prep_seed_shared_disk_pd.sh${RED} again${NC}"
-    exit 0
-fi 
-
 ## if the shared pd mount point does not exist, create it 
 if [[ -z "$(findmnt /dev/disk/by-id/google-shared-pd)" ]] 
 then 
@@ -47,8 +37,9 @@ fi
 ######################################## Data Prep Script ############################################### 
 echo -e "${RED}Hello World,  Insert your data prep scripts here ${NC}"
 echo -e "${RED}Store temporary data in  "${MOUNT_POINT}"/shared_pd/  ${NC}"
-sudo gsutil -m cp -r ${GCS_DATASET} ${MOUNT_POINT}/shared_pd/ 
 
+
+sudo gsutil -m cp -r ${GCS_DATASET} ${MOUNT_POINT}/shared_pd/ 
 ######################################### End of Data Prep Scrpit #######################################
 #########################################################################################################
 
